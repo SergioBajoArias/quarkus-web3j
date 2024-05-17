1ea5bf882bfa8c6753c5c55b9f49bcd9370b793f
Sergio Bajo <sergio.bajo@xeridia.com>
2024-05-17 13:08:06 +0200

    Initial commit

--- src/main/java/com/xeridia/config/ReflectionConfig.java
+++ src/main/java/com/xeridia/config/ReflectionConfig.java
@@ -3,7 +3,8 @@ package com.xeridia.config;
 import io.quarkus.runtime.annotations.RegisterForReflection;
 
 @RegisterForReflection(targets = {
-        org.web3j.protocol.core.Request.class
+        org.web3j.protocol.core.Request.class,
+        org.web3j.protocol.core.methods.response.EthGetTransactionCount.class
 })
 public class ReflectionConfig {
 }
--- src/test/java/com/xeridia/GanacheTestResourceLifecycleManager.java
+++ src/test/java/com/xeridia/GanacheTestResourceLifecycleManager.java
@@ -1,40 +1,70 @@
 package com.xeridia;
 
+import io.quarkus.test.common.DevServicesContext;
 import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
-import io.restassured.RestAssured;
-
+import lombok.extern.slf4j.Slf4j;
+import org.testcontainers.containers.DockerComposeContainer;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
-import org.testcontainers.utility.MountableFile;
 
+import java.io.File;
 import java.util.Map;
+import java.util.Optional;
 
-public class GanacheTestResourceLifecycleManager implements QuarkusTestResourceLifecycleManager {
+@Slf4j
+public class GanacheTestResourceLifecycleManager implements QuarkusTestResourceLifecycleManager, DevServicesContext.ContextAware {
 
     private static final String GANACHE_IMAGE = "trufflesuite/ganache";
 
     public static final int GANACHE_PORT = 8545;
+
+    private Optional<String> containerNetworkId = Optional.empty();
+
+    private DockerComposeContainer dockerComposeContainer;
+
     private GenericContainer container;
+
+    @Override
+    public void setIntegrationTestContext(DevServicesContext context) {
+        containerNetworkId = context.containerNetworkId();
+    }
 
     @SuppressWarnings("resource")
     @Override
     public Map<String, String> start() {
-        container = new GenericContainer(GANACHE_IMAGE)
-                .withCommand("-e 999999999999999999 --wallet.deterministic true")
-                .withExposedPorts(GANACHE_PORT)
-                .waitingFor(Wait.forListeningPort());
+        String blockchainNetwork;
+        if(containerNetworkId.isPresent()) {
+            dockerComposeContainer = new DockerComposeContainer(
+                    new File("src/test/resources/docker-compose-ganache.yml")
+            ).withLocalCompose(true);
 
-        container.start();
+            dockerComposeContainer.start();
+
+            blockchainNetwork = "http://ganache:" + GANACHE_PORT;
+        } else {
+            container = new GenericContainer(GANACHE_IMAGE)
+                    .withCommand("-e 999999999999999999 --wallet.deterministic true")
+                    .withExposedPorts(GANACHE_PORT)
+                    .waitingFor(Wait.forListeningPort());
+
+            container.start();
+
+            blockchainNetwork = "http://" + container.getHost() + ":" + container.getMappedPort(GANACHE_PORT);
+        }
+        log.info("Blockchain network is {}", blockchainNetwork);
 
         return Map.of(
-                "blockchain.network", "http://" + container.getHost() + ":" + container.getMappedPort(GANACHE_PORT)
+                "blockchain.network", blockchainNetwork
         );
     }
 
     @Override
     public void stop() {
-        if(container != null) {
-            container.stop();
+        if(this.container != null) {
+            this.container.stop();
+        }
+        if(this.dockerComposeContainer != null) {
+            this.dockerComposeContainer.stop();
         }
     }
 }
